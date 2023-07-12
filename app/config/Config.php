13a0e66d84ba272c63247e5509a84bf1c92c63e4
gilanghadi <gilangsaputro987@gmail.com>
2023-07-12 07:27:56 +0700

    [feat] initial commit

--- app/config/Config.php
+++ app/config/Config.php
@@ -6,4 +6,4 @@ define('BASEURL', 'http://localhost/perp');
 define('DBHOST', 'localhost');
 define('DBNAME', 'pertamini');
 define('DBUSER', 'root');
-define('DBPASS', 'addict0x021');
+define('DBPASS', '');
