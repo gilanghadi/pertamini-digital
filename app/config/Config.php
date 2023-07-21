b88fec19d68273b121884bfe38dc49d959d77f59
gilanghadi <gilangsaputro987@gmail.com>
2023-07-21 14:27:42 +0700

    [fix] gitignore

--- app/config/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-require './public/img/profile/path.php';
-define('BASEURL', 'http://localhost/perp');
-define('DBHOST', 'localhost');
-define('DBNAME', 'pertamini');
-define('DBUSER', 'root');
-define('DBPASS', 'addict0x021');
